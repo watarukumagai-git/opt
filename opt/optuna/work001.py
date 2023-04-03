e33c4b6470e79c2f65824504dba8f9224fb92d86
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2023-04-04 02:05:00 +0900

    Add files via upload

---
+++ opt/optuna/work001.py
@@ -0,0 +1,46 @@
+import optuna
+
+seed = 128
+num_variables = 2
+num_objs = 2
+# MOTPE, NSGAIISampler
+sampler_type = 'NSGAIISampler'
+
+def objective(trial):
+    obj = []
+    for j in range(1, num_objs + 1):
+        float_lower = 0.0
+        float_upper = 2.0 * j
+        x = trial.suggest_float(f'f{j}x1', float_lower, float_upper)
+        y = trial.suggest_float(f'f{j}x2', float_lower, float_upper)
+        z = trial.suggest_int(f'f{j}z1', 0, 1)
+        f = x**2 + y**2 + z**2
+        obj.append(f)
+    c0 = (x - 5) ** 2 + y**2 - 25
+    c1 = -((x - 8) ** 2) - (y + 3) ** 2 + 7.7
+    trial.set_user_attr("constraint", (c0, c1))
+    return obj
+
+def constraints(trial):
+    return trial.user_attrs["constraint"]
+
+def get_sampler(sampler_type):
+    if sampler_type == 'MOTPE':
+      n_startup_trials = 11 * num_objs - 1
+      n_trials = n_startup_trials + 10
+      sampler = optuna.samplers.MOTPESampler(
+          constraints_func=constraints,
+          n_startup_trials=n_startup_trials, n_ehvi_candidates=24, seed=seed
+      )
+    elif sampler_type == 'NSGAIISampler':
+      n_trials = 10
+      population_size = 50
+      sampler = optuna.samplers.NSGAIISampler(
+          constraints_func=constraints,
+          population_size=population_size, seed=seed
+      )
+    return sampler, n_trials
+
+(sampler, n_trials) = get_sampler(sampler_type)
+study = optuna.create_study(directions=["minimize"] * num_objs, sampler=sampler)
+study.optimize(objective, n_trials=n_trials)
