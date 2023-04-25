f8faeb4daf6098913a56bee67b7f8a7acad93ef1
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2023-04-26 03:15:52 +0900

    Add files via upload

--- opt/optuna/work001_1.py
+++ opt/optuna/work001_1.py
@@ -1,11 +1,13 @@
 import math
 import warnings
+import numpy as np
+import statistics 
 import optuna
 
 seed = 2
-n_trials = 100
-num_variables_r = 10
-num_variables_i = 0
+n_trials = 300
+num_variables_r = 5
+num_variables_i = 5
 num_variables = num_variables_r + num_variables_i
 l_var = [num_variables_r, num_variables_i, num_variables]
 
@@ -29,7 +31,9 @@ def def_var1():
     return ls
   
 def f1(x, c, d):
-    return (1/float(len(x))) * sum([(j-c)**2 for j in x]) - d
+    return statistics.mean(list(map(lambda j: (j-c) ** 2, x)))  - d
+    #return statistics.mean([(j-c)**2 for j in x])  - d
+    #return np.power(x_array-c, 2).mean - d
 
 
 class Prob:
@@ -39,17 +43,19 @@ class Prob:
         self.num_variables = l_var[2]
         self.float_lower = -5.0
         self.float_upper = 5.0
+        self.int_lower = 0
+        self.int_upper = 1
         self.d_1 = pow(10,-2)
         self.d_2 = pow(10,-2)
         self.gamma = pow(10, 3)
         self.x_opt = 1 - math.sqrt(self.d_2)
         self.obj_opt = self.x_opt**2
 
-    def def_var(self, trial):
+    def get_var(self, trial):
         if self.num_variables_r > 0:
             l_r = [trial.suggest_float(f'x{i}', self.float_lower, self.float_upper) for i in range(1, self.num_variables_r+1)]
         if self.num_variables_i > 0:
-            l_i = [trial.suggest_int(f'z{i}', 1, 2) for i in range(1, self.num_variables_i+1)]
+            l_i = [trial.suggest_int(f'z{i}', self.int_lower, self.int_upper) for i in range(1, self.num_variables_i+1)]
         if self.num_variables_r > 0 and self.num_variables_i > 0:
             x = l_r + l_i
         elif self.num_variables_r > 0 and self.num_variables_i == 0:
@@ -58,28 +64,34 @@ class Prob:
             x = l_i
         return x
 
-    def def_const(self, x):
+    def get_const(self, x):
         c0 = f1(x, 1, self.d_1)
         c1 = f1(x, 1, self.d_2)
-        #c0 = (1/float(self.num_variables)) * sum([(j-1)**2 for j in x]) - self.d_1
-        #c1 = (1/float(self.num_variables)) * sum([(j-2)**2 for j in x]) - self.d_2
-        c = (c0, c1)
+        c = [c0, c1]
+        #c = [c0]
         return c
 
     def objective(self, trial):
-        x = self.def_var(trial)
-        #obj = (1/float(self.num_variables)) * sum([j**2 for j in x])
-        c = self.def_const(x)
+        def get_vio(c):
+          return max(0, c)
+        x = self.get_var(trial)
+        c = self.get_const(x)
         # obj = f1(x, 0, 0)
-        obj = f1(x, 0, 0) + self.gamma * sum([cc for cc in c])
+        violation = sum(list(map(lambda cc: get_vio(cc), c)))
+        obj = f1(x, 0, 0) + self.gamma * violation
+        #if violation > 0:
+        #    obj = f1(x, 0, 0) + self.gamma * violation
+        #else:
+        #    obj = f1(x, 0, 0)
         trial.set_user_attr("constraint", c)
         return obj
 
-def constraints(trial):
-    return trial.user_attrs["constraint"]
 
-def get_sampler(sampler_type, constraints):
-    startup_trials = 200
+def get_sampler(sampler_type):
+    def constraints(trial):
+        return trial.user_attrs["constraint"]
+
+    startup_trials = 300
     if sampler_type == 'BoTorchSampler':
       sampler = optuna.integration.BoTorchSampler(
         constraints_func=constraints,
@@ -104,17 +116,17 @@ def get_sampler(sampler_type, constraints):
 Prob = Prob(l_var)
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=optuna.exceptions.ExperimentalWarning)    
-    sampler = get_sampler(sampler_type, constraints)
+    sampler = get_sampler(sampler_type)
     study = optuna.create_study(directions=["minimize"], sampler=sampler)
     study.optimize(Prob.objective, n_trials=n_trials)
 
 # but best_trials is only min_f not feasibility.
 for t in study.best_trials:
     print('Best trial: {:.0f}'.format(t.number))
-    print('User attrs1: {:.3f}'.format(t.user_attrs["constraint"][0]))
-    print('User attrs2: {:.3f}'.format(t.user_attrs["constraint"][1]))
     print('Best value orig: {:.6f}'.format(t.values[0]))
-    c = sum([cc for cc in t.user_attrs["constraint"]])
+    for i in range(len(t.user_attrs["constraint"])):
+        print('User attrs{0}: {1}'.format(i, t.user_attrs["constraint"][i]))
+    c = sum([max(0, cc) for cc in t.user_attrs["constraint"]])
     print('Best value: {:.6f}'.format(t.values[0] - Prob.gamma*c))
     for key, value in t.params.items():
         t.params[key] = round(t.params[key], 4)
