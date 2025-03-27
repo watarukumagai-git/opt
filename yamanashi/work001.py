c28dd74d5dbd47237f2d5ab72cd77743e5ba8a16
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2025-03-28 02:48:58 +0900

    Add files via upload

--- yamanashi/work001.py
+++ yamanashi/work001.py
@@ -32,7 +32,7 @@ def pdf_to_text(pdf_path, export_dir):
         first_page = pdf.pages[0]
 
         # テキストを抽出
-        text = first_page.extract_text()
+        text = first_page.extract_text().replace(' ', '')
         print(text)
     return text
 
@@ -51,14 +51,12 @@ def split_pdf_pages(src_path, dst_basepath):
 dir_base = path.dirname( path.abspath(__file__) )
 inout_dir = get_dir(dir_base)
 
-src_path = inout_dir[0] + '\\cover_v.pdf'
-dst_basepath = inout_dir[1] + '\\cover_v'
-pdf_list = split_pdf_pages(src_path, dst_basepath)
-
-src_path = inout_dir[0] + '\\' + 'cover_h.pdf'
-dst_basepath = inout_dir[1] + '\\cover_h'
-pdf_list2 = split_pdf_pages(src_path, dst_basepath)
-pdf_list = pdf_list + pdf_list2
+prefix_list = ['cover_v', 'cover_h']
+pdf_list = []
+for prefix in prefix_list:
+    src_path = inout_dir[0] + '\\' + prefix + '.pdf'
+    dst_basepath = inout_dir[1] + '\\' + prefix
+    pdf_list = pdf_list + split_pdf_pages(src_path, dst_basepath)
 
 #file_list = get_file_list(inout_dir[1] + '\\*')
 for filename in pdf_list:
@@ -67,38 +65,5 @@ for filename in pdf_list:
     pdfname = pdf_to_text(loadfile, export_dir)
     shutil.move(loadfile, inout_dir[1] + '\\' + pdfname + '_表紙.pdf')
 
-#(df_master, head_HHMM, tail_HHMM) = get_df_master(inout_dir, file_list)
-#d_order = {'morning': 0, 'afternoon': 1, 'evening': 2, 'night': 3}
-
-#clm_list = [('HH:MM', '30min'), ('timezone', 'timezone')]
-#period_list = [('yyyy/mm', '%Y-%m', 'month', 'M'), ('yyyy', '%Y', 'year', 'Y')]
-#file_head = inout_dir[1] + '\\spot_price_' + head_HHMM + '-' + tail_HHMM
-
-#for strftime1, strftime2, period, resample in period_list:
-#    # all average
-#    df_all = df_master.resample(resample, label='left').mean()
-#    df_all.to_csv(file_head + '_' + period + '.csv')
-#
-#for clm, name in clm_list:
-#    # all average in each time
-#    df_all_30min = df_master.groupby(clm).mean()
-#    if clm == 'timezone':
-#        df_all_30min = df_all_30min.reindex(index=['night', 'morning', 'afternoon', 'evening'])
-#    df_all_30min.to_csv(file_head + '_' + name + '_all.csv')
-#
-#    for strftime1, strftime2, period, resample in period_list:
-#        # all average in each month, year
-#        df_period = df_master.copy()
-#        df_period['timestamp'] = df_period.index
-#        df_period[strftime1] = df_period['timestamp'].dt.strftime(strftime2)
-#        #if clm == 'timezone' and strftime1 == 'yyyy/mm':
-#        #    df_period['yyyy'] = df_period['timestamp'].dt.strftime('%Y')
-#        df_period = df_period.groupby(by=[strftime1, clm], sort=False).mean()
-#        #if clm == 'timezone':
-#        #    if strftime1 == 'yyyy/mm':
-#        #        df_period = df_period.sort_values(by=['yyyy', strftime1, clm], key=lambda col: col.map(d_order))
-#        #    else:
-#        #        df_period = df_period.sort_values(by=[strftime1, clm], key=lambda col: col.map(d_order))
-#        df_period.to_csv(file_head + '_' + name + '_' + period + '.csv')
 
 print('finish')
