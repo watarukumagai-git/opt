44e91ac3c66208f511cd4e7d05deb3543afd0dd6
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2025-06-26 14:34:29 +0900

    Add files via upload

--- yamanashi/pdf/work003.py
+++ yamanashi/pdf/work003.py
@@ -12,8 +12,8 @@ def my_makedirs(path):
         os.makedirs(path)
 
 def get_dir(dir_base):
-    input_dir = dir_base + '\\input2'
-    output_dir = dir_base + '\\output2'
+    input_dir = dir_base + '\\input3'
+    output_dir = dir_base + '\\output3'
     my_makedirs(input_dir)
     my_makedirs(output_dir)
     return input_dir, output_dir
@@ -54,10 +54,39 @@ def pullout_pdf_pages(src_path, s, e):
         writer.add_page(reader.pages[i])
     writer.write(src_path)
 
+def change_password(
+    src_path, dst_path, src_password, dst_user_password=None, dst_owner_password=None
+):
+    src_pdf = pypdf.PdfReader(src_path)
+    src_pdf.decrypt(src_password)
+
+    dst_pdf = pypdf.PdfWriter()
+    dst_pdf.clone_reader_document_root(src_pdf)
+
+    d = {key: src_pdf.metadata[key] for key in src_pdf.metadata.keys()}
+    dst_pdf.add_metadata(d)
+
+    if dst_user_password is not None:
+        dst_pdf.encrypt(dst_user_password, dst_owner_password)
+
+    dst_pdf.write(dst_path)
+
+def merge_pdf(filename, dir1, dir2, outdir):
+    writer = pypdf.PdfWriter()
+    prefix = filename.split('.')[0]
+    # 表紙
+    writer.append(dir2 + '\\' + prefix + '_表紙.pdf')
+    # 本体
+    pdf_pass = pypdf.PdfReader(dir1 + '\\' + filename)
+    src_password = 'alliance'
+    change_password(dir1 + '\\' + filename, dir1 + '\\' + filename, src_password)
+    writer.append(dir1 + '\\' + filename)
+    writer.write(outdir + '\\' + filename)
+
 dir_base = path.dirname( path.abspath(__file__) )
 inout_dir = get_dir(dir_base)
 filelist = get_file_list(inout_dir[0] + '\\*')
 for file in filelist:
-    pullout_pdf_pages(inout_dir[0] + '\\' + file, 1, -1)
+    merge_pdf(file, inout_dir[0], dir_base + '\\output', inout_dir[1])
 
 print('finish')
